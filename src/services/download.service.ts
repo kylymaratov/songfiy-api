6c59bae15260a70c283dbde1c5a7ff789dcb737e
Kylym Maratov <kylymaratov@gmail.com>
2024-12-23 19:11:33 +0500

    initial commit

---
+++ src/services/download.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+//
+import { spawn } from 'node:child_process';
+
+@Injectable()
+export class DownloadService {
+  async getBufferById() {
+    try {
+      const buffer = await this.download();
+
+      return buffer;
+    } catch (error) {}
+  }
+
+  private download() {
+    return new Promise((resolve, reject) => {});
+  }
+}
---
+++ src/utils/password.util.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class PasswordUtil {
+  async generate() {}
+
+  async match() {}
+}
